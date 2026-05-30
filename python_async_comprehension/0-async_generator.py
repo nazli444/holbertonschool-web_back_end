14b49c1843efc330097ddf157da4d22bd7147d0a
Your Name <you@example.com>
2026-05-30 09:59:23 +0200

    README.md

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,12 +1,18 @@
 #!/usr/bin/env python3
-"""Async generator module"""
+"""
+This module contains an asynchronous generator coroutine.
+"""
 
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
-    """Yield 10 random numbers between 0 and 10, waiting 1 second each time."""
+async def async_generator() -> Generator[float, None, None]:
+    """
+    Coroutine that loops 10 times, asynchronously waits 1 second each time,
+    and yields a random floating-point number between 0 and 10.
+    """
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
