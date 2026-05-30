6c419d97b47ab02482782e4c9a06ab98aa2a8c6a
Your Name <you@example.com>
2026-05-30 09:57:41 +0200

    README.md

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
-"""Async generator with annotations"""
+"""Async generator module"""
 
 import asyncio
 import random
-from typing import Generator
 
 
-async def async_generator() -> Generator[float, None, None]:
+async def async_generator():
+    """Yield 10 random numbers between 0 and 10, waiting 1 second each time."""
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
