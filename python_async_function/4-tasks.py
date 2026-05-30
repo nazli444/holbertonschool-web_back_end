969418006d9f5a9ffb1022d312e57222d5d7f8c2
Your Name <you@example.com>
2026-05-30 08:57:06 +0200

    4-tasks.py

---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Tasks"""
+
+import asyncio
+from typing import List
+
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """Spawn task_wait_random n times and return delays in ascending order"""
+    delays = []
+
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+
+    for task in asyncio.as_completed(tasks):
+        delays.append(await task)
+
+    return delays
