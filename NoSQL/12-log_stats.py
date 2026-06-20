771cd58dd0d76118db0d30e54d8ff78111cadd99
Your Name <you@example.com>
2026-06-20 21:27:53 +0000

    12-log_stats.py

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -5,16 +5,16 @@ from pymongo import MongoClient
 
 
 def main():
+    """Print statistics about Nginx logs stored in MongoDB."""
     client = MongoClient("mongodb://127.0.0.1:27017")
     collection = client.logs.nginx
 
     total_logs = collection.count_documents({})
-
     print(f"{total_logs} logs")
+
     print("Methods:")
 
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-
     for method in methods:
         count = collection.count_documents({"method": method})
         print(f"\tmethod {method}: {count}")
@@ -22,7 +22,6 @@ def main():
     status_check = collection.count_documents(
         {"method": "GET", "path": "/status"}
     )
-
     print(f"{status_check} status check")
 
 
