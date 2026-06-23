c85ba9cc9f7ad2380bf2d69014577ca2db21a33a
Your Name <you@example.com>
2026-06-23 12:07:32 +0000

    2-arrow.js

---
+++ ES6_basic/2-arrow.js
@@ -0,0 +1,7 @@
+export default function getNeighborhoodsList() {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+  this.addNeighborhood = function add(newNeighborhood) {
+    this.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return self.sanFranciscoNeighborhoods;
+  };
+}
