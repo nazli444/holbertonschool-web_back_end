7bf0f8573ebaeddbb079f590be517dd10057e9f4
Your Name <you@example.com>
2026-08-13 19:44:40 +0000

    6-final-user.js

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -5,5 +5,17 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName),
-  ]);
+  ]).then((results) => results.map((result) => {
+    if (result.status === 'rejected') {
+      return {
+        status: result.status,
+        value: result.reason.toString(),
+      };
+    }
+
+    return {
+      status: result.status,
+      value: result.value,
+    };
+  }));
 }
