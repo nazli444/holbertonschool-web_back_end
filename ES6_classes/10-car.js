8db17d4ccf3d7956c3aaedf46de98939e9f39100
Your Name <you@example.com>
2026-08-13 20:17:30 +0000

    10-car.js

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -5,6 +5,10 @@ export default class Car {
     this._color = color;
   }
 
+  static get [Symbol.species]() {
+    return this;
+  }
+
   cloneCar() {
     return new this.constructor[Symbol.species]();
   }
